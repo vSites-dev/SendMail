c3b1bcc86b6f25970f89bf950c110a4b7126c2b6
varnaidvid <varnaidavid0522@gmail.com>
2025-01-06 09:17:05 +0100

    updated start-database script

--- start-database.sh
+++ start-database.sh
@@ -9,7 +9,7 @@
 
 # On Linux and macOS you can run this script directly - `./start-database.sh`
 
-DB_CONTAINER_NAME="leoai-dashboard-postgres"
+DB_CONTAINER_NAME="sendmail-postgres"
 
 if ! [ -x "$(command -v docker)" ]; then
   echo -e "Docker is not installed. Please install docker and try again.\nDocker install guide: https://docs.docker.com/engine/install/"
@@ -55,6 +55,6 @@ docker run -d \
   --name $DB_CONTAINER_NAME \
   -e POSTGRES_USER="postgres" \
   -e POSTGRES_PASSWORD="$DB_PASSWORD" \
-  -e POSTGRES_DB=leoai-dashboard \
+  -e POSTGRES_DB="sendmail-postgres" \
   -p "$DB_PORT":5432 \
   docker.io/postgres && echo "Database container '$DB_CONTAINER_NAME' was successfully created"
